0b4d2849baa7e83dd70690340bb7b2298d8256ec
val.istar.guo <val.istar.guo@gmail.com>
2021-08-02 23:52:55 +0800

    feat: add example page

--- templates/pages/index.vue
+++ templates/pages/index.vue
@@ -1,10 +1,29 @@
 <template>
-  <h1>Hellow Words</h1>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
 </template>
 <script lang="ts">
 export default {
   head() {
-    return { title: 'Val.istar.Guo' }
-  }
+    return { title: 'NuxtJS' }
+  },
+
+  data: () => ({ drawer: null })
 }
 </script>
